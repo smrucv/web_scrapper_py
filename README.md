3f873f928207ae594dd0643cd39d3ca2d9ffc959
Dimas Salazar <dimas.salazar@gmail.com>
2021-10-10 10:49:37 -0300

    API creation

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # web_scrapper_py
 News Service API from Dolar Today's site (Web Scrapper with python)
 
-Dolar Today's site: https://dolartoday.com/ News Service API: http://localhost:8000
+Dolar Today's site: https://dolartoday.com/ News Service API: http://localhost:5000
--- index.py
+++ index.py
@@ -1,17 +1,23 @@
 import requests
+import flask
+import json
 from bs4 import BeautifulSoup
 
 URL = "https://dolartoday.com"
 
-if __name__ == "__main__":
+app = flask.Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
     news = []
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
-    # print(soup.prettify())
 
     news = [{
-            "titulo" : new.select_one('div.post-title a')['title'],
-            "url" : "".join([URL, new.select_one('div.post-title a')['href']]),
-            "img" : new.select_one('div.post-image img')['src']
-    } for new in soup.select('.post.type-post') ]
-    print(news)
+        "titulo": new.select_one('div.post-title a')['title'],
+        "url": "".join([URL, new.select_one('div.post-title a')['href']]),
+        "img": new.select_one('div.post-image img')['src']
+    } for new in soup.select('.post.type-post')]
+    return json.dumps(news)
+
+app.run()
